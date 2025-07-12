fdd8f0c32bb5128b325320e3b3035244a94e9a5b
sidler2 <sidler2@sidler2.com>
2025-07-12 22:18:47 +0200

    CORS Changes
    Adding CORS AllowMethods, AllowHeaders, ExposeHeaders, MaxAge

--- internal/middlewares/cors.go
+++ internal/middlewares/cors.go
@@ -3,11 +3,16 @@ package middlewares
 import (
 	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
+	"time"
 )
 
 func CORSMiddleware() gin.HandlerFunc {
 	config := cors.DefaultConfig()
 	config.AllowOrigins = []string{"http://localhost:3000"} // Add your frontend URL
+	config.AllowMethods = []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"}
+	config.AllowHeaders = []string{"Origin", "Content-Type", "Authorization"}
+	config.ExposeHeaders = []string{"Content-Length"}
+	config.MaxAge = 12 * time.Hour
 	config.AllowCredentials = true
 	return cors.New(config)
 }
